7a86809eaf9f65e52a7b6181ea46228717b22be1
PatrykJankowski <patryk.jankowski@protonmail.com>
2022-04-12 17:29:30 +0200

    init

---
+++ src/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col center">
+                <h1>404</h1>
+                <hr>
+                <p>Bez formularzy i wpisywania danych.<br>Zamawiasz i płacisz w 10 sekund</p>
+                <a class="button" href="/">Wróć do strony głównej</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
