76bc9ff1bb1597a3536121e7c597c54704684c6d
PatrykJankowski <patryk.jankowski@protonmail.com>
2022-04-16 12:14:01 +0200

    homepage and mobile version updated

--- src/footer.php
+++ src/footer.php
@@ -3,23 +3,23 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 mb-20">
+            <div class="col-lg-3 mb-70 mb-20-lg text-align-center-lg">
                 <a href="/">
                     <svg width="200" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 223 49"><path d="M143.962 29.218c-3.002 0-5.722-2.373-5.722-6.63s2.72-6.538 5.722-6.538c3.049 0 5.724 2.339 5.724 6.59 0 4.253-2.675 6.59-5.724 6.59v-.012Zm5.724-16.11c-1.407-2.338-4.176-4.149-8.07-4.149-6.427 0-11.539 5.246-11.539 13.642 0 8.396 5.112 13.737 11.492 13.737 3.889 0 6.663-1.814 8.117-4.15v3.767h8.022V9.331h-8.022v3.778ZM177.219 29.218c-3.003 0-5.722-2.339-5.722-6.59 0-4.253 2.719-6.59 5.722-6.59 3.048 0 5.771 2.29 5.771 6.537 0 4.246-2.723 6.63-5.771 6.63v.013Zm2.393-20.285c-3.848 0-6.614 1.811-8.068 4.15V9.33h-8.037v39.304h8.037V32.223c1.454 2.24 4.22 4.101 8.068 4.101 6.426 0 11.539-5.343 11.539-13.736s-5.113-13.642-11.539-13.642M209.068 29.218c-3.002 0-5.722-2.339-5.722-6.59 0-4.253 2.72-6.59 5.722-6.59 3.049 0 5.771 2.29 5.771 6.537 0 4.246-2.722 6.63-5.771 6.63v.013Zm2.393-20.285c-3.847 0-6.614 1.811-8.068 4.15V9.33h-8.024v39.304h8.024V32.223c1.454 2.24 4.221 4.101 8.068 4.101 6.427 0 11.539-5.343 11.539-13.736s-5.112-13.642-11.539-13.642M108.079 12.856V9.33h-8.008v26.624h8.021V21.49c0-3.625 1.97-5.628 5.019-5.628 3.049 0 5.019 2.003 5.019 5.628v14.466h7.975V20.403c0-7.155-4.034-11.351-10.085-11.351-3.518 0-6.333 1.581-7.928 3.814l-.013-.01Zm-60.792 9.78c0-4.247 2.723-6.59 5.725-6.59 3.048 0 5.77 2.287 5.77 6.534s-2.721 6.63-5.77 6.63c-3.002 0-5.725-2.338-5.725-6.59v.015Zm.047-9.556V9.33h-8.021v39.301h8.021v-16.41c1.454 2.241 4.223 4.102 8.07 4.102 6.427 0 11.54-5.343 11.54-13.736S61.83 8.946 55.404 8.946c-3.847 0-6.616 1.811-8.07 4.15v-.016Zm35.37 2.388c2.673 0 4.925 1.671 4.925 4.581H77.59c.469-2.957 2.486-4.581 5.115-4.581Zm-13.136 7.167c0 8.436 5.535 13.69 13.221 13.69 6.52 0 11.164-4.007 12.431-9.206h-8.529c-.659 1.43-1.924 2.576-4.177 2.576-2.58 0-4.69-1.621-4.972-5.106H95.69c.091-.788.138-1.58.14-2.373 0-8.108-5.395-13.26-13.04-13.26-7.778 0-13.223 5.247-13.223 13.69v-.01ZM0 11.841h5.512V6.236H0v5.605Zm11.023 5.604h5.512v-5.604h5.444v5.51h2.964c1.161 1.08 1.941 2.82 1.941 5.271 0 4.482-2.672 6.63-5.535 6.63-2.815 0-5.185-2.02-5.332-6.213h-4.994v-5.594Zm0-16.813v11.209H5.512v11.21h2.333c.192 8.194 5.929 13.273 13.481 13.273 7.695 0 13.699-5.248 13.699-13.689 0-8.219-5.608-13.393-13.002-13.665V6.236h-5.488V.632h-5.512Z" fill="#fff"/><path d="M108.079 12.856V9.33h-8.008v26.624h8.021V21.49c0-3.625 1.97-5.628 5.019-5.628 3.049 0 5.019 2.003 5.019 5.628v14.466h7.975V20.403c0-7.155-4.034-11.351-10.085-11.351-3.518 0-6.333 1.581-7.928 3.814l-.013-.01Zm-60.792 9.78c0-4.247 2.723-6.59 5.725-6.59 3.048 0 5.77 2.287 5.77 6.534s-2.721 6.63-5.77 6.63c-3.002 0-5.725-2.338-5.725-6.59v.015Zm.047-9.556V9.33h-8.021v39.301h8.021v-16.41c1.454 2.241 4.223 4.102 8.07 4.102 6.427 0 11.54-5.343 11.54-13.736S61.83 8.946 55.404 8.946c-3.847 0-6.616 1.811-8.07 4.15v-.016Zm35.37 2.388c2.673 0 4.925 1.671 4.925 4.581H77.59c.469-2.957 2.486-4.581 5.115-4.581Zm-13.136 7.167c0 8.436 5.535 13.69 13.221 13.69 6.52 0 11.164-4.007 12.431-9.206h-8.529c-.659 1.43-1.924 2.576-4.177 2.576-2.58 0-4.69-1.621-4.972-5.106H95.69c.091-.788.138-1.58.14-2.373 0-8.108-5.395-13.26-13.04-13.26-7.778 0-13.223 5.247-13.223 13.69v-.01ZM0 11.841h5.512V6.236H0v5.605Zm11.023 5.604h5.512v-5.604h5.444v5.51h2.964c1.161 1.08 1.941 2.82 1.941 5.271 0 4.482-2.672 6.63-5.535 6.63-2.815 0-5.185-2.02-5.332-6.213h-4.994v-5.594Zm0-16.813v11.209H5.512v11.21h2.333c.192 8.194 5.929 13.273 13.481 13.273 7.695 0 13.699-5.248 13.699-13.689 0-8.219-5.608-13.393-13.002-13.665V6.236h-5.488V.632h-5.512Z" fill="#fff"/></svg>
                 </a>
             </div>
-            <div class="col-lg-3 mb-20 color-white font-weight-700 font-size-20">
-                <div class="color-white mb-20">kontakt <div class="button-icon button-icon--white"><div class="arrow arrow--rotate-90 arrow--button arrow--button-down arrow--white"></div></div></div>
+            <div class="col-lg-3 mb-20 color-white font-size-20 text-align-center-lg">
+                <div class="font-weight-700 mb-20">kontakt <div class="button-icon button-icon--white"><div class="arrow arrow--rotate-90 arrow--button arrow--button-down arrow--white"></div></div></div>
                 <div>555 555 555</div>
-                <div>aopenapp@gmail.com</div>
+                <div>kontakt@open-app.com</div>
             </div>
-            <div class="col-lg-3 mb-20 color-white font-weight-700 font-size-20">
-                <div class="mb-20">adres <div class="button-icon button-icon--white"><div class="arrow arrow--rotate-90 arrow--button arrow--button-down arrow--white"></div></div></div>
+            <div class="col-lg-3 mb-20 color-white font-size-20 text-align-center-lg">
+                <div class="font-weight-700 mb-20">adres <div class="button-icon button-icon--white"><div class="arrow arrow--rotate-90 arrow--button arrow--button-down arrow--white"></div></div></div>
                 <div>OpenApp Sp. z o.o.</div>
                 <div>ul. Prosta 20</div>
                 <div>00-850 Warszawa</div>
             </div>
-            <div class="col-lg-3 color-white font-weight-700 font-size-20">
+            <div class="col-lg-3 color-white font-weight-700 font-size-20 text-align-center-lg">
                 <?php wp_nav_menu(array('theme_location' => 'nav-menu-footer', 'container' => false)); ?>
             </div>
         </div>
@@ -32,6 +32,7 @@
 </footer>
 
 <script>
+    // mobile menu
     let navOpened = false;
     let initHeight = 360;
 
@@ -49,6 +50,23 @@
         }
     }
 
+
+    // navbar bg change on scroll
+    let scrollpos = window.scrollY
+    const header = document.querySelector(".navbar")
+    const header_height = header.offsetHeight
+
+    const add_class_on_scroll = () => header.classList.add("navbar--bg")
+    const remove_class_on_scroll = () => header.classList.remove("navbar--bg")
+
+    window.addEventListener('scroll', function() {
+        scrollpos = window.scrollY;
+        if (scrollpos >= header_height) { add_class_on_scroll() }
+        else { remove_class_on_scroll() }
+    })
+
+
+    // accordion
     let acc = document.getElementsByClassName("accordion");
     let i;
 
