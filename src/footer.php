64a7dd1b085c776d35830b931434689a34f78474
PatrykJankowski <patryk.jankowski@protonmail.com>
2022-04-19 13:40:55 +0200

    mobile version, styles and screens updated

--- src/footer.php
+++ src/footer.php
@@ -43,10 +43,12 @@
             navOpened = false;
             navMobile.style.height = '0';
             document.getElementById('navbar__toggle').classList.remove('navbar__toggle--open');
+            document.getElementById('header').classList.remove('navbar--bg-on-toggle');
         } else {
             navOpened = true;
             navMobile.style.height = initHeight + 'px';
             document.getElementById('navbar__toggle').classList.add('navbar__toggle--open');
+            document.getElementById('header').classList.add('navbar--bg-on-toggle');
         }
     }
 
